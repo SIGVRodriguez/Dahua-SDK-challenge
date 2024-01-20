3481878b6d1817ce52f677f1de46ac139264f0f0
SIGVRodriguez <victor.rodriguez@sig.systems>
2024-01-20 11:14:16 -0600

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.login_button = new System.Windows.Forms.Button();
+            this.pwd_textBox = new System.Windows.Forms.TextBox();
+            this.name_textbox = new System.Windows.Forms.TextBox();
+            this.port_textBox = new System.Windows.Forms.TextBox();
+            this.ip_textBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelU = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.save_button = new System.Windows.Forms.Button();
+            this.comboBox_channel = new System.Windows.Forms.ComboBox();
+            this.snap_button = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox_preview = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.login_button);
+            this.panel1.Controls.Add(this.pwd_textBox);
+            this.panel1.Controls.Add(this.name_textbox);
+            this.panel1.Controls.Add(this.port_textBox);
+            this.panel1.Controls.Add(this.ip_textBox);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.labelU);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(8, 11);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(241, 184);
+            this.panel1.TabIndex = 0;
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(53, 141);
+            this.login_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(99, 32);
+            this.login_button.TabIndex = 9;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.login_button_Click);
+            // 
+            // pwd_textBox
+            // 
+            this.pwd_textBox.Location = new System.Drawing.Point(83, 109);
+            this.pwd_textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pwd_textBox.Name = "pwd_textBox";
+            this.pwd_textBox.Size = new System.Drawing.Size(149, 22);
+            this.pwd_textBox.TabIndex = 8;
+            this.pwd_textBox.Text = "admin123";
+            this.pwd_textBox.UseSystemPasswordChar = true;
+            // 
+            // name_textbox
+            // 
+            this.name_textbox.Location = new System.Drawing.Point(83, 78);
+            this.name_textbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.name_textbox.Name = "name_textbox";
+            this.name_textbox.Size = new System.Drawing.Size(149, 22);
+            this.name_textbox.TabIndex = 7;
+            this.name_textbox.Text = "admin";
+            // 
+            // port_textBox
+            // 
+            this.port_textBox.Location = new System.Drawing.Point(83, 49);
+            this.port_textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.port_textBox.Name = "port_textBox";
+            this.port_textBox.Size = new System.Drawing.Size(149, 22);
+            this.port_textBox.TabIndex = 6;
+            this.port_textBox.Text = "37777";
+            this.port_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.port_textBox_KeyPress);
+            // 
+            // ip_textBox
+            // 
+            this.ip_textBox.Location = new System.Drawing.Point(83, 20);
+            this.ip_textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ip_textBox.MaxLength = 16;
+            this.ip_textBox.Name = "ip_textBox";
+            this.ip_textBox.Size = new System.Drawing.Size(149, 22);
+            this.ip_textBox.TabIndex = 5;
+            this.ip_textBox.Text = "192.168.1.1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password :";
+            // 
+            // labelU
+            // 
+            this.labelU.AutoSize = true;
+            this.labelU.Location = new System.Drawing.Point(9, 84);
+            this.labelU.Name = "labelU";
+            this.labelU.Size = new System.Drawing.Size(42, 16);
+            this.labelU.TabIndex = 3;
+            this.labelU.Text = "User :";
+            this.labelU.Click += new System.EventHandler(this.labelU_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Port :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Device Login";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.save_button);
+            this.panel2.Controls.Add(this.comboBox_channel);
+            this.panel2.Controls.Add(this.snap_button);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Location = new System.Drawing.Point(8, 555);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(241, 176);
+            this.panel2.TabIndex = 10;
+            // 
+            // save_button
+            // 
+            this.save_button.Location = new System.Drawing.Point(136, 142);
+            this.save_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(94, 32);
+            this.save_button.TabIndex = 13;
+            this.save_button.Text = "Save";
+            this.save_button.UseVisualStyleBackColor = true;
+            // 
+            // comboBox_channel
+            // 
+            this.comboBox_channel.FormattingEnabled = true;
+            this.comboBox_channel.Location = new System.Drawing.Point(79, 15);
+            this.comboBox_channel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox_channel.Name = "comboBox_channel";
+            this.comboBox_channel.Size = new System.Drawing.Size(151, 24);
+            this.comboBox_channel.TabIndex = 11;
+            // 
+            // snap_button
+            // 
+            this.snap_button.Location = new System.Drawing.Point(4, 142);
+            this.snap_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.snap_button.Name = "snap_button";
+            this.snap_button.Size = new System.Drawing.Size(94, 32);
+            this.snap_button.TabIndex = 9;
+            this.snap_button.Text = "Snap";
+            this.snap_button.UseVisualStyleBackColor = true;
+            this.snap_button.Click += new System.EventHandler(this.snap_button_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(9, 22);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 16);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Channel :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(9, 3);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(64, 16);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Snapshot";
+            // 
+            // pictureBox_preview
+            // 
+            this.pictureBox_preview.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox_preview.Location = new System.Drawing.Point(255, 11);
+            this.pictureBox_preview.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox_preview.Name = "pictureBox_preview";
+            this.pictureBox_preview.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBox_preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_preview.TabIndex = 11;
+            this.pictureBox_preview.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1542, 737);
+            this.Controls.Add(this.pictureBox_preview);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Dahua SDK challenge";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_preview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button login_button;
+        private TextBox pwd_textBox;
+        private TextBox name_textbox;
+        private TextBox port_textBox;
+        private TextBox ip_textBox;
+        private Label label5;
+        private Label labelU;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Panel panel2;
+        private Button snap_button;
+        private Label label9;
+        private Label label10;
+        private ComboBox comboBox_channel;
+        private Button save_button;
+        private PictureBox pictureBox_preview;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using NetSDKCS;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+
+        private IntPtr _LoginID = IntPtr.Zero;
+        private IntPtr _PlayID = IntPtr.Zero;
+        private int _snapped = 0;
+        private fSnapRevCallBack _SnapRevCallBack;
+        private NET_DEVICEINFO_Ex _DeviceInfo = new NET_DEVICEINFO_Ex();
+        private bool _IsSpanCapture = false;
+        private DateTime m_DateTimeNow;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                //_SnapRevCallBack = new fSnapRevCallBack(SnapRevCallBack);
+                NETClient.Init(null, IntPtr.Zero, null);
+                NETClient.SetSnapRevCallBack(_SnapRevCallBack, IntPtr.Zero);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Process.GetCurrentProcess().Kill();
+            }
+            this.Load += new EventHandler(Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitOrLogoutUI();
+        }
+
+        private void InitOrLogoutUI()
+        {
+            login_button.Text = "Login";
+            snap_button.Enabled = false;
+            save_button.Enabled = false;
+            comboBox_channel.Enabled = false;
+            comboBox_channel.Items.Clear();
+            this.pictureBox_preview.Image = null;
+            pictureBox_preview.Refresh();
+
+        }
+
+
+
+
+        private void login_button_Click(object sender, EventArgs e)
+        {
+
+            if (IntPtr.Zero == _LoginID)
+            {
+                ushort port = 0;
+                try
+                {
+                    port = Convert.ToUInt16(this.port_textBox.Text.Trim());
+                }
+                catch
+                {
+                    MessageBox.Show("Input port error");
+                    return;
+                }
+                _LoginID = NETClient.LoginWithHighLevelSecurity(this.ip_textBox.Text.Trim(), port, this.name_textbox.Text.Trim(), this.pwd_textBox.Text, EM_LOGIN_SPAC_CAP_TYPE.TCP, IntPtr.Zero, ref _DeviceInfo);
+                if (IntPtr.Zero == _LoginID)
+                {
+                    MessageBox.Show(NETClient.GetLastError());
+                    return;
+                }
+                for (int i = 0; i < _DeviceInfo.nChanNum; i++)
+                {
+                    this.comboBox_channel.Items.Add(i + 1);
+                }
+                this.comboBox_channel.SelectedIndex = 0;
+                this.comboBox_channel.Enabled = true;
+                this.snap_button.Enabled = true;
+                this.login_button.Text = "Logout";
+                this.pictureBox_preview.Refresh();
+                this.startplay();
+
+            }
+            else
+            {
+                NETClient.Logout(_LoginID);
+                _LoginID = IntPtr.Zero;
+                this.InitOrLogoutUI();
+            }
+        }
+        private void startplay()
+        {
+            if (IntPtr.Zero == _PlayID)
+            {
+                _PlayID = NETClient.RealPlay(_LoginID, comboBox_channel.SelectedIndex, this.pictureBox_preview.Handle);
+                if (IntPtr.Zero == _PlayID)
+                {
+                    MessageBox.Show(NETClient.GetLastError());
+                    return;
+                }
+            }
+            else
+            {
+                NETClient.StopRealPlay(_PlayID);
+                _PlayID = IntPtr.Zero;
+                this.pictureBox_preview.Image?.Dispose();
+                this.pictureBox_preview.Refresh();
+
+            }
+        }
+
+        private void snap_button_Click(object sender, EventArgs e)
+        {
+            if (_snapped == 0)
+            {
+                
+                if (pictureBox_preview.Image != null)
+                {
+                    pictureBox_preview.Image.Dispose();
+                }
+                string path = AppDomain.CurrentDomain.BaseDirectory + "image";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                DateTime now = DateTime.Now;
+                string filePath = path + "\\" + string.Format("{0}-{1}-{2}-{3}-{4}-{5}", now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second) + ".jpg";
+                string filePath2 = path + "\\" + string.Format("{0}-{1}-{2}-{3}-{4}-{5}", now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second+1) + ".jpg";
+                bool ret = NETClient.CapturePicture(_PlayID, filePath, EM_NET_CAPTURE_FORMATS.JPEG);
+                Thread.Sleep(1000);
+                bool ret2 = NETClient.CapturePicture(_PlayID, filePath2, EM_NET_CAPTURE_FORMATS.JPEG);
+                if (!ret || !ret2)
+                {
+                    MessageBox.Show(NETClient.GetLastError());
+                    return;
+                }
+                Image image = Image.FromFile(filePath);
+                NETClient.StopRealPlay(_PlayID);
+                _PlayID = IntPtr.Zero;
+                //this.pictureBox_preview.Refresh();
+                pictureBox_preview.Image = image;
+                MessageBox.Show("Pictures saved!");
+                snap_button.Text = "Resume";
+                _snapped = 1;
+            }
+            else
+            {
+                snap_button.Text = "Snap";
+                startplay();
+                _snapped = 0;
+            }
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            NETClient.Cleanup();
+        }
+
+        private void port_textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 8 && !Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void labelU_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
